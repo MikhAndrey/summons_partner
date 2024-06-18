89550f94b3d85c15ed1e16f29f1f8f3aee417409
MikhAndrey <mikhalevange@gmail.com>
2024-06-18 15:50:53 +0300

    Initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="App-header">
+            <div className="App-header-info">
+                <img className="App-header-logo" src="./summons_partner_logo.png"/>
+                <span className="App-header-name-label">SUMMONS PARTNER+</span>
+            </div>
+            <div className="App-header-menu">
+                <div className="App-header-menu-items">
+                    <a href="#" className="App-header-menu-item">How it works</a>
+                    <a href="#" className="App-header-menu-item">Features</a>
+                    <a href="#" className="App-header-menu-item">Reviews</a>
+                    <a href="#" className="App-header-menu-item">Help</a>
+                    <a href="#" className="App-header-menu-item">Contacts</a>
+                </div>
+                <div>
+                    <button className="App-header-menu-start-button">Get started free</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Main.tsx
@@ -0,0 +1,38 @@
+import Header from "./Header";
+import React from "react";
+
+function Main() {
+    return (
+        <div className="App-main">
+            <Header></Header>
+            <div className="App-main-label">
+                <div className="App-main-label-primary">
+                    Get the ultimate
+                </div>
+                <div className="App-main-label-primary">
+                    all-in-one solution
+                </div>
+                <div className="App-main-label-secondary">
+                    for police officers in New York State.
+                </div>
+            </div>
+            <div className="App-main-mobile-screen-container">
+                <img className="App-main-mobile-screen-image" src="./summons_mobile_screen.png" alt="Mobile screen"/>
+            </div>
+            <div className="App-main-city-image-outer-container">
+                <div className="App-main-city-image-inner-container">
+                    <div className="App-main-app-download">
+                        <span className="App-main-app-download-label">Download the App</span>
+                        <img className="App-main-app-download-icon" src="./app_download.png" alt="Download app"/>
+                    </div>
+                </div>
+                <img className="App-main-city-image" src="./city_image.png" alt="City image"/>
+            </div>
+            <div className="App-qr-code-image-container">
+                <img className="App-qr-code-image" src="./qr_code.png" alt="QR code"/>
+            </div>
+        </div>
+    );
+}
+
+export default Main;

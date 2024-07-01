feea24ab7add7d377ef98516d0d131a1bdb1f8e0
MKade19 <kadetov51@gmail.com>
2024-07-01 17:17:33 +0300

    add-footer-links

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,12 +1,14 @@
 function Footer() {
+    const termsOfUseUrl = 'https://arttica.is/terms';
+
     return (
         <div className="App-footer">
             <div className="App-footer-copyright">
                 © 2020-2024 n.u. logistics - All Rights Reserved
             </div>
             <div className="App-footer-links-container">
-                <div className="App-footer-link">Terms of use</div>
-                <div className="App-footer-link">Privacy policy</div>
+                <a target="_blank" href={ termsOfUseUrl } className="App-footer-link">Terms of use</a>
+                <a target="_blank" href={ termsOfUseUrl } className="App-footer-link">Privacy policy</a>
             </div>
         </div>
     )
